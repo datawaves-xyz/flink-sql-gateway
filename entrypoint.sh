82d63b050ff63b656f3ee561afdeb742c1fb27ce
TalkWithKeyboard <610347922@qq.com>
2022-09-30 15:46:22 +0800

    fix: error conf file name

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-echo "jobmanager.rpc.address: $1\
-jobmanager.rpc.port: 6123\
-kubernetes.cluster-id: $2\
-execution.target: kubernetes-session" > conf/flink_conf.yaml
+echo "jobmanager.rpc.address: $1\n
+jobmanager.rpc.port: 6123\n
+kubernetes.cluster-id: $2\n
+execution.target: kubernetes-session" > conf/flink-conf.yaml
 
 ./flink-sql-gateway/bin/sql-gateway.sh
